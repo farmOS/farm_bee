81920005586a65eb309c31dad8a69a0c5bdb1389
Michael Stenta <mike@mstenta.net>
2021-01-04 14:14:44 -0500

    Add Bee Colony asset type.

---
+++ src/Plugin/Asset/AssetType/BeeColony.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_bee\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the bee colony asset type.
+ *
+ * @AssetType(
+ *   id = "bee_colony",
+ *   label = @Translation("Bee colony"),
+ * )
+ */
+class BeeColony extends FarmAssetType {
+
+}
